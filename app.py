39b83a8f1c68efe12acd830f7ecba81e0d677895
Fyke Lleva <110708999+f9ki3@users.noreply.github.com>
2025-05-23 22:04:23 +0800

    updated new ai

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for    
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return render_template('/pages/index.html')
+
+@app.route('/login')
+def login():
+    return render_template('/pages/login.html')
+
+@app.route('/register')
+def register():
+    return render_template('/pages/register.html')
+
+@app.route('/scan')
+def scan():
+    return render_template('/pages/scan.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('/pages/settings.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)
